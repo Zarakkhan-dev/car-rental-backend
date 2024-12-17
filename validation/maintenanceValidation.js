578e3c4a82dc7f57937f895b7fcac08d038d63ef
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2024-12-18 00:16:21 +0500

    Set backend maintaince

--- validation/maintenanceValidation.js
+++ validation/maintenanceValidation.js
@@ -13,53 +13,44 @@ const maintenanceValidationSchema = Joi.object({
     'any.required': 'Date is required.'
   }),
 
-  chassis_no: Joi.string().optional().messages({
+  chassis_no: Joi.string().optional().allow(null).messages({
     'string.base': 'Chassis number should be a string.'
   }),
 
-  engine: Joi.string().optional().messages({
+  engine: Joi.string().optional().allow(null).messages({
     'string.base': 'Engine should be a string.'
   }),
 
-  reg_no: Joi.string().optional().messages({
-    'string.base': 'Registration number should be a string.'
-  }),
-
-  cell: Joi.string().optional().messages({
+  cell: Joi.string().optional().allow(null).messages({
     'string.base': 'Cell should be a string.'
   }),
 
-  type: Joi.string().optional().messages({
+  type: Joi.string().optional().allow(null).messages({
     'string.base': 'Type should be a string.'
   }),
 
-  labour: Joi.array().items(
-    Joi.object({
-      description: Joi.string().required(),
-      cost: Joi.number().precision(2).required().messages({
-        'number.base': 'Labour cost should be a number.',
-        'number.precision': 'Labour cost should have up to two decimal places.'
-      })
-    })
-  ).optional().messages({
-    'array.base': 'Labour should be an array of objects.'
+  labour: Joi.string().optional().allow(null).messages({
+    'string.base': 'Labour should be a string.'
   }),
 
-  total_labour_cost: Joi.number().precision(2).required().messages({
+  total_labour_cost: Joi.number().precision(2).positive().required().messages({
     'number.base': 'Total labour cost should be a number.',
     'number.precision': 'Total labour cost should have up to two decimal places.',
+    'number.positive': 'Total labour cost should be a positive number.',
     'any.required': 'Total labour cost is required.'
   }),
 
-  total_parts_cost: Joi.number().precision(2).required().messages({
+  total_parts_cost: Joi.number().precision(2).positive().required().messages({
     'number.base': 'Total parts cost should be a number.',
     'number.precision': 'Total parts cost should have up to two decimal places.',
+    'number.positive': 'Total parts cost should be a positive number.',
     'any.required': 'Total parts cost is required.'
   }),
 
-  grand_total: Joi.number().precision(2).required().messages({
+  grand_total: Joi.number().precision(2).positive().required().messages({
     'number.base': 'Grand total should be a number.',
     'number.precision': 'Grand total should have up to two decimal places.',
+    'number.positive': 'Grand total should be a positive number.',
     'any.required': 'Grand total is required.'
   })
 });
