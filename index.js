c8d12a4cf301a3247c89c0b04c84b258c86a159e
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2024-12-18 20:10:25 +0500

    dfda

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import cors from "cors";
-import Mainroute from "./routes/mainRoutes.js";
+import userRoute from "./routes/carRoutes.js"
+import bookingRoutes from "./routes/bookingRoutes.js";
+import carRoutes from "./routes/carRoutes.js";
+import cardRoutes from "./routes/cardRoutes.js ";
+import driverRoutes from "./routes/driverRoutes.js"
+import fuelingRoutes from "./routes/fuelingRoutes.js";
+import maintenanceRoutes from "./routes/maintenanceRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import schedule from 'node-schedule';
+import {sendPreDueDateReminder ,sendDueDateAlert } from "./Controller/notificationController.js";
 
 configDotenv(); // Load environment variables from .env file
 
@@ -16,9 +25,30 @@ app.get("/", (req, res) => {
   res.send("Car Rental API .... !");
 });
 
-// Main API routes
-app.use("/v0/api", Mainroute);
+app.use("/user",userRoute)
+app.use("/cars",carRoutes);
+app.use("/bookings",bookingRoutes);
+app.use("/cards",cardRoutes);
+app.use("/drivers",driverRoutes);
+app.use("/fuelings",fuelingRoutes);
+app.use("/maintenances",maintenanceRoutes);
+app.use("/notifications",notificationRoutes)
+schedule.scheduleJob('0 8 * * *', async () => {
+  try {
+    await sendPreDueDateReminder();
+  } catch (error) {
+    console.error('Error sending pre-due date reminders:', error);
+  }
+});
 
+// Schedule due date alerts to run every day at 8 AM
+schedule.scheduleJob('0 8 * * *', async () => {
+  try {
+    await sendDueDateAlert();
+  } catch (error) {
+    console.error('Error sending due date alerts:', error);
+  }
+});
 app.listen(process.env.PORT,()=>{
   console.log("server is running")
 })
