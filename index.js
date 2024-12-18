5e05a79dc99c3591fbbc1a1bc9333b1c26c36132
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2024-12-18 20:21:06 +0500

    asd

--- index.js
+++ index.js
@@ -33,22 +33,24 @@ app.use("/drivers",driverRoutes);
 app.use("/fuelings",fuelingRoutes);
 app.use("/maintenances",maintenanceRoutes);
 app.use("/notifications",notificationRoutes)
-schedule.scheduleJob('0 8 * * *', async () => {
-  try {
-    await sendPreDueDateReminder();
-  } catch (error) {
-    console.error('Error sending pre-due date reminders:', error);
-  }
-});
 
-// Schedule due date alerts to run every day at 8 AM
-schedule.scheduleJob('0 8 * * *', async () => {
-  try {
-    await sendDueDateAlert();
-  } catch (error) {
-    console.error('Error sending due date alerts:', error);
-  }
-});
+
+// schedule.scheduleJob('0 8 * * *', async () => {
+//   try {
+//     await sendPreDueDateReminder();
+//   } catch (error) {
+//     console.error('Error sending pre-due date reminders:', error);
+//   }
+// });
+
+// // Schedule due date alerts to run every day at 8 AM
+// schedule.scheduleJob('0 8 * * *', async () => {
+//   try {
+//     await sendDueDateAlert();
+//   } catch (error) {
+//     console.error('Error sending due date alerts:', error);
+//   }
+// });
 app.listen(process.env.PORT,()=>{
   console.log("server is running")
 })
